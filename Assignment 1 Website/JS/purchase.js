8e280440c6d49fbd71613071483701f767e2a195
Sarah Xu <67710129+uphria@users.noreply.github.com>
2023-01-31 13:18:39 -0500

    Add files via upload

---
+++ Assignment 1 Website/JS/purchase.js
@@ -0,0 +1,63 @@
+function myFunction() { /*I added JavaScript for the Purchase form to make it more interactive. I used online sources to find how to code JavaScript for my specific purpose. My variables are checkBox and text. I have conditions - if its checked, it will the show the information I coded in HTML, if not, nothing will happen.*/
+  var checkBox = document.getElementById("myCheck");
+  var text = document.getElementById("text");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function myFunction1() {
+  var checkBox = document.getElementById("myCheck1");
+  var text = document.getElementById("text1");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function myFunction2() {
+  var checkBox = document.getElementById("myCheck2");
+  var text = document.getElementById("text2");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function myFunction3() {
+  var checkBox = document.getElementById("myCheck3");
+  var text = document.getElementById("text3");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function myFunction4() {
+  var checkBox = document.getElementById("myCheck4");
+  var text = document.getElementById("text4");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function myFunction5() {
+  var checkBox = document.getElementById("myCheck5");
+  var text = document.getElementById("text5");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function myFunction6() {
+  var checkBox = document.getElementById("myCheck6");
+  var text = document.getElementById("text6");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
